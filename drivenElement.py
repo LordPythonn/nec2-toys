79aafd8a5bef1af1d1a5e082145100e1e253b95d
wsnook <will@willsnook.com>
2012-10-18 22:11:48 -0500

    working on building driven element with formulas

--- drivenElement.py
+++ drivenElement.py
@@ -2,7 +2,10 @@
 Copyright 2012 Will Snook (http://willsnook.com)
 MIT License
 
-Use utility functions to generate a NEC2 card stack file for what might be a folded dipole
+Use utility functions to generate a NEC2 card stack file for the driven element of a 2 element 2m
+Cheap Yagi design. My goal is to produce a model that matches the 150 Ohm free space impedance
+at 145MHz which Kent Britain mentioned in his "Cheap Antennas for the AMSAT LEO's" article. Assuming
+I get to that point, I intend to finish out the antenna and see how the SWR looks across the 2m band.
 '''
 
 from nec2utils import *
@@ -29,10 +32,29 @@ EN     0     0     0      0  0.00000E+00  0.00000E+00  0.00000E+00  0.00000E+00 
 
 
 # =======================================================================================================
-# Make the wire cards
+# Make the wire cards to model the driven element of a Cheap Yagi 2 element, 2m beam.
 # =======================================================================================================
 
-wireRadius = m(0.001585) # Radius = 1/16", diameter = 1/8"
+'''
+Wires and Arcs of the driven element that I'm trying to model...
+- The '+' characters mark segment boundaries
+- A, B, C, E, F, G, and H are 1/8" diameter wires (one GW card for each)
+- D is an 1/8" diameter arc which requires a combination of a GA card and a couple GM cards to model
+- I is a single segment wire to model the feedpoint (GW card with an EX card linking to the GW's tag number)
+
+
+                 A                        B                         C                      
+-------------------------------------+--------+-------------------------------------------+-,
+                                              |                                              \
+                                            I |                                               \
+                                              |                                               | D
+                                     ---------+-----+___                                      /
+                                         H       G      ---___                               /
+                                                        F     +---------------------------+-'
+                                                                            E
+'''
+
+wireRadius = inch(1.0/16.0) # Radius = 1/16", diameter = 1/8"
 segs = 15                  # This is what xnec2c came up with
 feedpointSegs = 1          # My reading of the EZNEC's feed point docs suggests this is correct
 
@@ -50,7 +72,7 @@ wires += gm(deg(0),deg(0),deg(0),m(-0.4826),m(0.1778),m(0.09365),6)
 # Write the file
 # =======================================================================================================
 
-fileName = 'foldedDiPy2.nec'
+fileName = 'freeSpace2mDE.nec'
 writeCardsToFile(fileName, comments, wires, footer)
 copyCardFileToConsole(fileName)
 
