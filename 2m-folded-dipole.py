d46edd47eddfee2ba107f92884d893f34106318c
wsnook <will@willsnook.com>
2012-10-29 02:55:58 -0500

    Modeled 146.310 MHz folded dipole: 5.6 SWR, 278 Ohm feedpoint impedance

--- 2m-folded-dipole.py
+++ 2m-folded-dipole.py
@@ -9,94 +9,69 @@ from nec2utils import *
 
 
 # =======================================================================================================
-# Make the wire cards to model the driven element of a Cheap Yagi 2 element, 2m beam.
+# Plan for a 2m folded dipole
 # =======================================================================================================
 
 '''
-Wires and Arcs of the driven element that I'm trying to model...
-- The '+' characters mark boundaries between GW or GA cards in the model
-- A, B, D are 1/8" diameter wires (one GW card for each)
-- C is an arc of 1/8" diameter wire, center at c, radius of rc (GA card + a GM card to position it)
-- E is a single segment wire to model the feedpoint (GW card + an EX card, linked by tag #)
-- In terms of the GW card docs, a1 is the point (XW1, YW1, ZW1), a2 is (XW2, YW2, ZW2), and
-  so on for b1, b2, ... (see http://www.nec2.org/part_3/cards/gw.html)
-- Origin is where a2 meets b1 and e2
-- Coordinate system is what xnec2c uses... putting the back of your right hand against your
-  monitor with your thumb pointed up, your index finger pointed left, and your middle
-  finger pointed at you... thumb is +Z, index is +X, middle is +Y
+Notes:
+  '+' marks a boundary between wire elements
+  'X' marks the feedpoint
+  Material is 1/8" diameter bare copper wire
+  Total length of wire is 1 wavelength of center frequency
+  Wavelength = (300 * velocity factor of 1/8" bare copper) / (design frequency)
+  A = (1/2 wavelength) - (pi * rb)
 
+        a1                                   A                                   a2 
+     ,-+---------------------------------------------------------------------------+-,
+    /                                                                                 \
+ D |rd d                                                                           b rb| B
+    \                                                                                 /
+     `-+-------------------------------------X-------------------------------------+-'
+        c2                                   C                                   c1
 
-a1               A                    a2  b1                      B                     b2 
------------------------------------------+------------------------------------------------+-,
-a1                                       |e2                                                 \
-                                       E |                                                c rc| C
-                                         |e1                                                 /
-                                         +------------------------------------------------+-'
-                                          d2                      D                     d1
-Constraints:
-  Total Length of wire in driven element = 3/4 wavelength at target frequency
-  A + B + (pi * rc) + D = 3/4 wavelength of target frequency (length of DE)
-  A = B + (pi * rc) + D
-  E is less than the width of the wooden beam so that holes can be drilled for the wires (must be <= 0.5 inches)
 '''
 
-refY				= 0.0
-refZ                = inch(24.0)
-refLength           = inch(40.0 + (2.0/8.0))
+targetMHz        = 146.310
+correctionFactor = 0.932   # Trial and error correcion constant to account for velocity factor, etc
+wavelength       = m((300.0 * correctionFactor) / targetMHz)
 
-targetMHz           = 146.310
-velocityFactor      = 0.937   # This is what seems to work from trial and error simulations with 1/8" wires
-quarterWavelength   = m((300.0 * 0.937) / targetMHz) / 4.0 
-arcRadiusC          = inch(0.25)
-deY                 = inch(5.0 + (2.0/8.0))
-deTopZ              = refZ
-bLength             = ((2.0*quarterWavelength) - (math.pi*arcRadiusC)) / 2.0
+radiusB = inch(0.5)
+radiusD = radiusB
+A       = (0.5 * wavelength) - (math.pi*radiusB)
+C       = A
+Y0      = inch(5.0 + (2.0/8.0))
+Z0      = inch(36.0)
 
-comments  = 'CM ----------------------------------------------------------------------------------\n'
-comments += 'CM  NEC2 model for simulating a 2-element 2m Yagi built from copper wire on a wooden\n'
-comments += 'CM  beam in the style of WA5VJB\'s Cheap Yagi designs.\n'
+comments  = 'CM -------------------------------------------------------------------\n'
+comments += 'CM  NEC2 model for simulating a folded dipole built from copper wire.\n'
+comments += 'CM  Geometry is tuned for min SWR at {:.3f} MHz\n'.format(targetMHz)
 comments += 'CM  \n'
-comments += 'CM  Driven element (DE) geometry, reflector (REF) to DE spacing, and REF length are\n'
-comments += 'CM  tuned for min SWR at {:.3f} MHz\n'.format(targetMHz)
-comments += 'CM  \n'
-comments += 'CM  REF length                     = {: >6.3f} in\n'.format(mToIn(refLength))
-comments += 'CM  REF to DE spacing              = {: >6.3f} in\n'.format(mToIn(deY))
-comments += 'CM  DE length before bending the J = {: >6.3f} in\n'.format(mToIn(3.0*quarterWavelength))
-comments += 'CM  Unbent end of DE to feedpoint  = {: >6.3f} in\n'.format(mToIn(quarterWavelength))
-comments += 'CM  Radius of bend in J            = {: >6.3f} in\n'.format(mToIn(arcRadiusC))
-comments += 'CM ----------------------------------------------------------------------------------\n'
+comments += 'CM  Wire length before bends = {: >6.3f} in\n'.format(mToIn(wavelength))
+comments += 'CM  Radius of bends          = {: >6.3f} in\n'.format(mToIn(radiusB))
+comments += 'CM -------------------------------------------------------------------\n'
 comments += 'CE'
 
-a1   = Point(quarterWavelength, deY, deTopZ)
-a2   = Point(0.0, deY, deTopZ)
-b1   = a2
-b2   = Point(-bLength, b1.y, b1.z)
-c    = Point(b2.x, deY, deTopZ-arcRadiusC)
-d1   = Point(b2.x, deY, deTopZ-(2.0*arcRadiusC))
-d2   = Point(a2.x, deY, d1.z)
-e1   = d2
-e2   = a2
-
+a1 = Point(A/2.0, Y0, Z0)
+a2 = Point(-a1.x, Y0, Z0)
+b  = Point( a2.x, Y0, Z0-radiusB)
+c1 = Point( a2.x, Y0, Z0-(2.0*radiusB))
+c2 = Point( a1.x, Y0, c1.z)
+d  = Point( a1.x, Y0, b.z)
 
 wireRadius = inch(1.0/16.0) # radius for a 1/8" wire
-segs = 25
-refSegs = 51
-arcSegs = 15
-arcStart = deg(90)
-arcEnd   = deg(270)
-feedpointSegs = 1           # My reading of the EZNEC's feed point docs suggests this is how to attach coax
+segs       = 51
+arcSegs    = 15
+arcStart   = deg(90)
+arcEnd     = deg(270)
 
 m = Model(wireRadius)
-
-# Driven element
 m.addWire(segs, a1, a2)
-m.addWire(segs, b1, b2)
-m.addArc(arcSegs, arcRadiusC, arcStart, arcEnd, rotate=Rotation(deg(0),deg(0),deg(0)), translate=c)
-m.addWire(segs, d1, d2)
-m.addWire(feedpointSegs, e1, e2).attachToEX()
+m.addArc(arcSegs, radiusB, arcStart, arcEnd, rotate=Rotation(deg(0),deg(0),deg(0)), translate=b)
+m.addWire(segs, c1, c2).feedAtMiddle()
+m.addArc(arcSegs, radiusD, arcStart, arcEnd, rotate=Rotation(deg(0),deg(0),deg(180)), translate=d)
 
-cardStack = m.getText(start=145.710, stepSize=0.05, stepCount=30)
-
+steps = (148.0 - 144.0) / 0.1
+cardStack = m.getText(start=144.000, stepSize=0.1, stepCount=steps)
 
 
 # =======================================================================================================
@@ -104,7 +79,7 @@ cardStack = m.getText(start=145.710, stepSize=0.05, stepCount=30)
 # =======================================================================================================
 
 fileName = '2m-folded-dipole.nec'
-writeCardsToFile(fileName, comments, cardStack, "")
+writeCardsToFile(fileName, comments, cardStack)
 copyCardFileToConsole(fileName)
 
 
--- nec2utils.py
+++ nec2utils.py
@@ -21,7 +21,7 @@ def sci(f):
 def dec(i):
 	''' Return formatted string containing a decimal integer in a 6 char wide field (tags, segments)
 	'''
-	return '{: >6d}'.format(i)
+	return '{: >6d}'.format(math.trunc(i))
 
 
 # =======================================================================================================
@@ -60,98 +60,6 @@ def mToIn(meters):
 # Tag & segments have no units. Dimensions are in meters. Angles are in degrees.
 # =======================================================================================================
 
-def gw(tag, segments, x1, y1, z1, x2, y2, z2, radius):
-	''' Return the line for a GW card, a wire.
-	'''
-	gw = "GW" + dec(tag) + dec(segments)
-	gw += sci(x1) + sci(y1) + sci(z1)
-	gw += sci(x2) + sci(y2) + sci(z2)
-	gw += sci(radius) + "\n"
-	return gw
-
-def ga(tag, segments, arcRadius, startAngle, endAngle, wireRadius):
-	''' Return the line for a GA card, an arc in the X-Z plane with its center at the origin
-	'''
-	notUsed = 0.0
-	ga = "GA" + dec(tag) + dec(segments)
-	ga += sci(arcRadius) + sci(startAngle) + sci(endAngle)
-	ga += sci(wireRadius)
-	ga += sci(notUsed) # Note: xnec2c fills this in with its "Segs % lambda" field, but that may be a bug
-	ga += sci(notUsed) + sci(notUsed) + "\n"
-	return ga
-
-def gm(rotX, rotY, rotZ, trX, trY, trZ, firstTag):
-	''' Return the line for a GM card, move (rotate and translate).
-		rotX, rotY, and rotZ: angle to rotate around each axis
-		trX, trY, and trZ: distance to translate along each axis
-		firstTag: first tag# to apply transform to (subseqent tag#'s get it too... like it or not)
-	'''
-	tagIncrement = 0
-	newStructures = 0
-	gm = "GM" + dec(tagIncrement) + dec(newStructures)
-	gm += sci(rotX) + sci(rotY) + sci(rotZ)
-	gm += sci(trX) + sci(trY) + sci(trZ)
-	gm += sci(firstTag*1.0) + "\n"
-	return gm
-
-def ge():
-	''' Card to "terminate reading of geometry data cards"
-	'''
-	GPFLAG = 0  # Ground plane flag. 0 means no ground plane present.
-	ge = "GE"
-	ge += dec(GPFLAG) + "\n"
-	return ge
-
-def fr(start, stepSize, stepCount):
-	''' Define the frequency range to be modeled
-	'''
-	IFRQ = 0           # Step type, 0 is linear (additive), 1 = multiplicative
-	NFRQ = stepCount   # Number of frequency steps
-	I3   = 0           # blank
-	I4   = 0           # blank
-	FMHZ   = start     # Starting frequency in MHz
-	DELFRQ = stepSize  # Frequency stepping increment (IFRQ=0), or multiplication factor (IFRQ=1)
-	fr = "FR"
-	fr += dec(IFRQ) + dec(NFRQ) + dec(I3) + dec(I4)
-	fr += sci(FMHZ) + sci(DELFRQ) + "\n"
-	return fr
-
-def ex(tag,segment):
-	''' Define excitation parameters.
-	'''
-	I1 = 0        # Excitation type. 0 means an "applied-E-field" voltage source
-	I2 = tag      # Tag number of the wire element to which the source will be applied
-	I3 = segment  # Segment within the previously specified wire element to which the source will be applied
-	I4 = 0        # 0 means use defaults for admittance matrix asymmetry and printing input impedance voltage
-	F1 = 1.0      # Real part of voltage
-	F2 = 0.0      # Imaginary part of voltage
-	ex = "EX"
-	ex += dec(I1) + dec(I2) + dec(I3) + dec(I4)
-	ex += sci(F1) + sci(F2) + "\n"
-	return ex
-
-
-def rp():
-	''' Card to initiate calculation and output of radiation pattern.
-	'''
-	I1  = 0      # 0 is normal mode: defaults to free-space unless a previous GN card specified a ground plane
-	NTH = 37     # Number of values of theta (angle away from positive Z axis)
-	NPH = 37     # Number of values of phi (angle away from X axis in the XY plane)
-	I4  = 0      # Use defaults for some misc output printing options
-	THETS = 0.0  # Theta start value in degrees
-	PHIS  = 0.0  # Phi start value in degrees
-	DTH   = 10.0 # Delta-theta in degrees
-	DPH   = 10.0 # Delta-phi in degrees
-	rp = "RP"
-	rp += dec(I1) + dec(NTH) + dec(NPH) + dec(I4)
-	rp += sci(THETS) + sci(PHIS) + sci(DTH) + sci(DPH) + "\n"
-	return rp
-
-
-def en():
-	''' Card to mark end of input
-	'''
-	return "EN\n"
 
 
 # =======================================================================================================
@@ -184,13 +92,127 @@ class Model:
 		self.transforms = ""
 		self.wireRadius = wireRadius
 		self.tag        = 0
-		self.tagForEX   = 0
+		self.EX_tag     = 0
+		self.EX_segment = 0
 
-	def addWire(self, segments, point1, point2):
+		self.transformBuffer = ''
+
+	# ---------------------------------------------------------------------------------------------------
+	# Low-level card emitter functions
+	# ---------------------------------------------------------------------------------------------------
+
+	def flushTransformBuffer(self):
+		''' Used in some song and dance to avoid the edge case that can occur with an arc as the last element
+		    My double GM card trick causes a problem if the second GM tries to refer to a tag that doesn't exist
+		'''
+		self.transforms      += self.transformBuffer
+		self.transformBuffer  = ""
+
+
+	def gw(self, tag, segments, x1, y1, z1, x2, y2, z2, radius):
+		''' Return the line for a GW card, a wire.
+		'''
+		gw = "GW" + dec(tag) + dec(segments)
+		gw += sci(x1) + sci(y1) + sci(z1)
+		gw += sci(x2) + sci(y2) + sci(z2)
+		gw += sci(radius) + "\n"
+		return gw
+
+	def ga(self, tag, segments, arcRadius, startAngle, endAngle, wireRadius):
+		''' Return the line for a GA card, an arc in the X-Z plane with its center at the origin
+		'''
+		notUsed = 0.0
+		ga = "GA" + dec(tag) + dec(segments)
+		ga += sci(arcRadius) + sci(startAngle) + sci(endAngle)
+		ga += sci(wireRadius)
+		ga += sci(notUsed) # Note: xnec2c fills this in with its "Segs % lambda" field, but that may be a bug
+		ga += sci(notUsed) + sci(notUsed) + "\n"
+		return ga
+
+	def gm(self, rotX, rotY, rotZ, trX, trY, trZ, firstTag):
+		''' Return the line for a GM card, move (rotate and translate).
+			rotX, rotY, and rotZ: angle to rotate around each axis
+			trX, trY, and trZ: distance to translate along each axis
+			firstTag: first tag# to apply transform to (subseqent tag#'s get it too... like it or not)
+		'''
+		tagIncrement = 0
+		newStructures = 0
+		gm = "GM" + dec(tagIncrement) + dec(newStructures)
+		gm += sci(rotX) + sci(rotY) + sci(rotZ)
+		gm += sci(trX) + sci(trY) + sci(trZ)
+		gm += sci(firstTag*1.0) + "\n"
+		return gm
+
+	def ge(self):
+		''' Card to "terminate reading of geometry data cards"
+		'''
+		GPFLAG = 0  # Ground plane flag. 0 means no ground plane present.
+		ge = "GE"
+		ge += dec(GPFLAG) + "\n"
+		return ge
+
+	def fr(self, start, stepSize, stepCount):
+		''' Define the frequency range to be modeled
+		'''
+		IFRQ = 0           # Step type, 0 is linear (additive), 1 = multiplicative
+		NFRQ = stepCount   # Number of frequency steps
+		I3   = 0           # blank
+		I4   = 0           # blank
+		FMHZ   = start     # Starting frequency in MHz
+		DELFRQ = stepSize  # Frequency stepping increment (IFRQ=0), or multiplication factor (IFRQ=1)
+		fr = "FR"
+		fr += dec(IFRQ) + dec(NFRQ) + dec(I3) + dec(I4)
+		fr += sci(FMHZ) + sci(DELFRQ) + "\n"
+		return fr
+
+	def ex(self,tag,segment):
+		''' Define excitation parameters.
+		'''
+		I1 = 0        # Excitation type. 0 means an "applied-E-field" voltage source
+		I2 = tag      # Tag number of the wire element to which the source will be applied
+		I3 = segment  # Segment within the previously specified wire element to which the source will be applied
+		I4 = 0        # 0 means use defaults for admittance matrix asymmetry and printing input impedance voltage
+		F1 = 1.0      # Real part of voltage
+		F2 = 0.0      # Imaginary part of voltage
+		ex = "EX"
+		ex += dec(I1) + dec(I2) + dec(I3) + dec(I4)
+		ex += sci(F1) + sci(F2) + "\n"
+		return ex
+
+
+	def rp(self):
+		''' Card to initiate calculation and output of radiation pattern.
+		'''
+		I1  = 0      # 0 is normal mode: defaults to free-space unless a previous GN card specified a ground plane
+		NTH = 37     # Number of values of theta (angle away from positive Z axis)
+		NPH = 37     # Number of values of phi (angle away from X axis in the XY plane)
+		I4  = 0      # Use defaults for some misc output printing options
+		THETS = 0.0  # Theta start value in degrees
+		PHIS  = 0.0  # Phi start value in degrees
+		DTH   = 10.0 # Delta-theta in degrees
+		DPH   = 10.0 # Delta-phi in degrees
+		rp = "RP"
+		rp += dec(I1) + dec(NTH) + dec(NPH) + dec(I4)
+		rp += sci(THETS) + sci(PHIS) + sci(DTH) + sci(DPH) + "\n"
+		return rp
+
+
+	def en(self):
+		''' Card to mark end of input
+		'''
+		return "EN\n"
+
+	# ---------------------------------------------------------------------------------------------------
+	# High-level geometry functions
+	# ---------------------------------------------------------------------------------------------------
+
+	def addWire(self, segments, pt1, pt2):
 		''' Append a wire, increment the tag number, and return this object to facilitate a chained attachToEX() call
 		'''
 		self.tag += 1
-		self.wires += gw(self.tag, segments, point1.x, point1.y, point1.z, point2.x, point2.y, point2.z, self.wireRadius)
+		self.wires += self.gw(self.tag, segments, pt1.x, pt1.y, pt1.z, pt2.x, pt2.y, pt2.z, self.wireRadius)
+		self.flushTransformBuffer()
+		self.middle = math.trunc(segments/2) + 1
 		return self
 
 	def addArc(self, segments, radius, start, end, rotate, translate):
@@ -200,24 +222,29 @@ class Model:
 		'''
 		# Place the arc in the XZ plane with its center on the origin
 		self.tag += 1
-		self.wires += ga(self.tag, segments, radius, start, end, self.wireRadius)
+		self.wires += self.ga(self.tag, segments, radius, start, end, self.wireRadius)
+		self.flushTransformBuffer()
+		self.middle = math.trunc(segments/2) + 1
 		# Move the arc to where it's supposed to be (note the tag #)
-		self.transforms += gm(rotate.rx, rotate.ry, rotate.rz, translate.x, translate.y, translate.z, self.tag)
+		self.transforms += self.gm(rotate.rx, rotate.ry, rotate.rz, translate.x, translate.y, translate.z, self.tag)
 		# Restore the coordinate system (note the use of tag # + 1)
-		self.transforms += gm(-rotate.rx, -rotate.ry, -rotate.rz, -translate.x, -translate.y, -translate.z, self.tag+1)
+		self.transformBuffer = \
+			self.gm(-rotate.rx, -rotate.ry, -rotate.rz, -translate.x, -translate.y, -translate.z, self.tag+1)
 		return self
 
-	def attachToEX(self):
-		''' Tell the model to remember the current tag number for use with the EX voltage source card
+	def feedAtMiddle(self):
+		''' Attach the EX card feedpoint to the middle segment of the element that was most recently created
 		'''
-		self.tagForEX = self.tag
+		self.EX_tag     = self.tag
+		self.EX_segment = self.middle
+
 
 	def getText(self, start, stepSize, stepCount):
-		footer = ge()
-		footer += ex(tag=self.tagForEX, segment=1)
-		footer += fr(start, stepSize, stepCount)
-		footer += rp()
-		footer += en()
+		footer = self.ge()
+		footer += self.ex(tag=self.EX_tag, segment=self.EX_segment)
+		footer += self.fr(start, stepSize, stepCount)
+		footer += self.rp()
+		footer += self.en()
 		return self.wires + self.transforms + footer
 
 
@@ -225,13 +252,12 @@ class Model:
 # File I/O
 # =======================================================================================================
 
-def writeCardsToFile(fileName, comments, wires, footer):
+def writeCardsToFile(fileName, comments, cardStack):
 	''' Write a NEC2 formatted card stack to the output file
 	'''
 	nec2File = open(fileName,'w')
 	nec2File.write(comments.strip() + "\n")
-	nec2File.write(   wires.strip() + "\n")
-	nec2File.write(  footer.strip() + "\n")
+	nec2File.write(cardStack.strip() + "\n")
 	nec2File.close()
 
 
