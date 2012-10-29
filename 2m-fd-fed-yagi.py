9787f65e182fff8e5b279562e43a6b875435fdbf
wsnook <will@willsnook.com>
2012-10-29 05:37:42 -0500

    2m yagi fed with in-plane folded dipole, 6.5 dbi gain and 1.04 SWR @ 146.310 MHz

--- 2m-fd-fed-yagi.py
+++ 2m-fd-fed-yagi.py
@@ -41,8 +41,7 @@ Notes:
 '''
 
 targetMHz        = 146.310
-#correctionFactor = 0.932   # Trial and error correcion constant to account for velocity factor, etc
-correctionFactor = 0.9347   # Trial and error correcion constant to account for velocity factor, etc
+correctionFactor = 0.9374   # Trial and error correcion constant to account for velocity factor, etc
 wavelength       = m((300.0 * correctionFactor) / targetMHz)
 
 # Folded dipole driven element
@@ -50,7 +49,6 @@ radiusB = inch(1.0)
 radiusD = radiusB
 A       = (0.5 * wavelength) - (math.pi*radiusB)
 C       = A
-#Y0      = inch(5.0 + (2.0/8.0))
 Y0      = inch(5.0 + (3.0/8.0))
 Z0      = inch(36.0)
 
@@ -69,15 +67,15 @@ comments += 'CM ----------------------------------------------------------------
 comments += 'CE'
 
 # DE
-a1 = Point(A/2.0, Y0, Z0)
-a2 = Point(-a1.x, Y0, Z0)
-b  = Point( a2.x, Y0, Z0-radiusB)
-c1 = Point( a2.x, Y0, Z0-(2.0*radiusB))
-c2 = Point( a1.x, Y0, c1.z)
-d  = Point( a1.x, Y0, b.z)
+a1 = Point(A/2.0, Y0-radiusB, Z0)
+a2 = Point(-a1.x, Y0-radiusB, Z0)
+b  = Point( a2.x, Y0,         Z0)
+c1 = Point( a2.x, Y0+radiusB, Z0)
+c2 = Point( a1.x, Y0+radiusB, Z0)
+d  = Point( a1.x, Y0,         Z0)
 # Ref
-e1 = Point( E/2.0, Y1, b.z)
-e2 = Point(-E/2.0, Y1, b.z)
+e1 = Point( E/2.0, Y1, Z0)
+e2 = Point(-E/2.0, Y1, Z0)
 
 wireRadius = inch(1.0/16.0) # radius for a 1/8" wire
 segs       = 41
@@ -88,9 +86,9 @@ arcEnd     = deg(270)
 m = Model(wireRadius)
 # Driven element
 m.addWire(segs, a1, a2)
-m.addArc(arcSegs, radiusB, start=deg(90), end=deg(270), rotate=Rotation(deg(0),deg(0),deg(0)), translate=b)
+m.addArc(arcSegs, radiusB, start=deg(90), end=deg(270), rotate=Rotation(deg(90),deg(0),deg(0)), translate=b)
 m.addWire(segs, c1, c2).feedAtMiddle()
-m.addArc(arcSegs, radiusD, arcStart, arcEnd, rotate=Rotation(deg(0),deg(0),deg(180)), translate=d)
+m.addArc(arcSegs, radiusD, arcStart, arcEnd, rotate=Rotation(deg(90),deg(0),deg(180)), translate=d)
 # Reflector
 m.addWire(segs, e1, e2)
 
